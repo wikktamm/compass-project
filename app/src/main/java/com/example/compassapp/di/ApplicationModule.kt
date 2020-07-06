ec3f76207497d111cafdcaed0f8b42266007ee24
Wiktoria Tarnowska <wikktamm@gmail.com>
2020-07-06 10:06:47 +0200

    Fix issue with fragment dupliaction

--- app/src/main/java/com/example/compassapp/di/ApplicationModule.kt
+++ app/src/main/java/com/example/compassapp/di/ApplicationModule.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import com.example.compassapp.data.orientation.CompassOrientationSource
 import com.example.compassapp.data.orientation.OrientationSource
 import com.example.compassapp.logic.RxSensors
-import com.patloew.rxlocation.RxLocation
+//import com.patloew.rxlocation.RxLocation
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,10 +16,10 @@ import javax.inject.Singleton
 @InstallIn(ApplicationComponent::class)
 class ApplicationModule {
 
-    @Singleton
-    @Provides
-    fun provideRxLocationClient(@ApplicationContext context: Context): RxLocation =
-        RxLocation(context)
+//    @Singleton
+//    @Provides
+//    fun provideRxLocationClient(@ApplicationContext context: Context): RxLocation =
+//        RxLocation(context)
 
     @Singleton
     @Provides
--- app/src/main/java/com/example/compassapp/ui/CompassActivity.kt
+++ app/src/main/java/com/example/compassapp/ui/CompassActivity.kt
@@ -1,14 +1,9 @@
 package com.example.compassapp.ui
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.example.compassapp.R
-import com.example.compassapp.utils.Constants.PERMISSION_FINE_COARSE
 import com.example.compassapp.viewmodels.CompassViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,50 +15,13 @@ class CompassActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_compass)
-        if (checkForPermissions()) {
-            displayCompassFragment()
-        } else requestPermissions()
+        displayCompassFragment(savedInstanceState == null)
     }
 
-    private fun displayCompassFragment() {
-        supportFragmentManager.beginTransaction().add(R.id.fragmentContainer, CompassFragment())
-            .commit()
-    }
-
-    private fun checkForPermissions() =
-        isFineLocationGranted() && isCoarseLocationGranted()
-
-
-    private fun requestPermissions() {
-        ActivityCompat.requestPermissions(
-            this, arrayOf(
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ), PERMISSION_FINE_COARSE
-        )
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        if (requestCode == PERMISSION_FINE_COARSE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                displayCompassFragment()
-            } else requestPermissions()
+    private fun displayCompassFragment(shouldCreate: Boolean) {
+        if (shouldCreate) {
+            supportFragmentManager.beginTransaction().add(R.id.fragmentContainer, CompassFragment())
+                .commit()
         }
     }
-
-    private fun isFineLocationGranted() =
-        ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.ACCESS_FINE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
-
-
-    private fun isCoarseLocationGranted() = ContextCompat.checkSelfPermission(
-        this,
-        Manifest.permission.ACCESS_COARSE_LOCATION
-    ) == PackageManager.PERMISSION_GRANTED
 }
